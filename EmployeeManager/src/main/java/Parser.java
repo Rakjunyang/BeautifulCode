25d89ce514601cd6c7200cc235835bc5eaff61b5
jiyun <wldbs9211@nate.com>
2022-03-18 16:23:39 +0900

    feat: Add Parser

--- EmployeeManager/src/main/java/Parser.java
+++ EmployeeManager/src/main/java/Parser.java
@@ -1,6 +1,14 @@
-public class Parser {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
-    public static void parse(String cmd) {
-        // TODO: parse cmd 구현
+public class Parser {
+    public static ArrayList<String> parse(String str) {
+        String validPattern = "^[a-zA-Z0-9\\s\\,\\-]+$";
+
+        if (Pattern.matches(validPattern, str)){
+            return new ArrayList<>(Arrays.asList(str.split(",")));
+        }
+        return null;
     }
 }
--- EmployeeManager/src/test/java/ParserTest.java
+++ EmployeeManager/src/test/java/ParserTest.java
@@ -1,3 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
 public class ParserTest {
 
+    @Test
+    void validStringTest() {
+        assertNotNull(new Parser().parse("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
+        assertNotNull(new Parser().parse("SCH,-p,-d, ,birthday,04"));
+        assertNotNull(new Parser().parse("DEL, , , ,employeeNum,18115040"));
+        assertNotNull(new Parser().parse("MOD,-p, , ,employeeNum,08123556,birthday,20110706"));
+
+        assertNull(new Parser().parse("DEL, , , ,name,홍길동"));
+        assertNull(new Parser().parse("DEL, , , ,employeeNum,18115040@"));
+    }
 }
