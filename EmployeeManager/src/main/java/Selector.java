8b24644d1281decd722fa4f7c0708cacfd1acbfc
rakjun <lakjuny1@gmail.com>
2022-03-18 16:35:05 +0900

    remove unused files

--- EmployeeManager/src/main/java/Selector.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class Selector {
-    public ArrayList<Employee> selectEmployees(OptionSelector optionSelector, String selectKey, String selectValue){
-        return null;
-    }
-}
