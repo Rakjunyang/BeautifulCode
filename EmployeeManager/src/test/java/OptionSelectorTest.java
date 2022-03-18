57f216cdfecaf22796458343050adf49e9effaa2
정병훈 <101618457+Harvey-JJ@users.noreply.github.com>
2022-03-18 16:56:44 +0900

    Option Selector Test Code
    Option Selector Test Code 추가

---
+++ EmployeeManager/src/test/java/OptionSelectorTest.java
@@ -0,0 +1,145 @@
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OptionSelectorTest {
+    EmployeeManager employeeManager;
+    ArrayList<Employee> employees;
+
+    @BeforeEach
+    void setUp(){
+        employeeManager = new EmployeeManager();
+        employeeManager.add(new Employee("12345600", "first last",  "cl2", "010-1234-5678", "19920312", "PRO"));
+        employeeManager.add(new Employee("12345601", "abc def",  "cl3", "010-1234-2222", "20220318", "ADV"));
+        employeeManager.add(new Employee("00111234", "one man",  "cl4", "010-2222-2222", "19700018", "ADV"));
+        employeeManager.sort();
+        employees = employeeManager.getEmployees();
+    }
+
+    @Test
+    void idDefaultTest(){
+        OptionSelector optionSelector = new DefaultOptionSelector("id", "12345601");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 1);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("id"), "12345601");
+    }
+
+    @Test
+    void clDefaultTest(){
+        OptionSelector optionSelector = new DefaultOptionSelector("cl", "cl4");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 1);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("cl"), "cl4");
+    }
+
+    @Test
+    void firstNameTest(){
+        OptionSelector optionSelector = new FirstNameOptionSelector("first last");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 1);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("name").split(" ")[0], "first");
+    }
+
+    @Test
+    void lastNameTest(){
+        OptionSelector optionSelector = new LastNameOptionSelector("first last");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 1);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("name").split(" ")[1], "last");
+    }
+
+    @Test
+    void midPhoneNumberTest(){
+        OptionSelector optionSelector = new MidPhoneNumberOptionSelector("010-1234-5678");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 2);
+        Assertions.assertEquals(matchedEmployees.get(1).getValue("phoneNumber").split("-")[1], "1234");
+    }
+
+    @Test
+    void lastPhoneNumberTest(){
+        OptionSelector optionSelector = new LastPhoneNumberOptionSelector("010-2222-2222");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 2);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("phoneNumber").split("-")[2], "2222");
+    }
+
+    @Test
+    void YearBirthDayTest(){
+        OptionSelector optionSelector = new YearBirthDayOptionSelector("20220318");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 1);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("birthDay"), "20220318");
+    }
+
+    @Test
+    void MonthBirthDayTest(){
+        OptionSelector optionSelector = new MonthBirthDayOptionSelector("20220318");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 2);
+        Assertions.assertEquals(matchedEmployees.get(1).getValue("birthDay"), "20220318");
+    }
+
+    @Test
+    void DayBirthDayTest(){
+        OptionSelector optionSelector = new DayBirthDayOptionSelector("20220318");
+        ArrayList<Employee> matchedEmployees = new ArrayList<>();
+
+        for(Employee employee : employees){
+            if(optionSelector.match(employee))
+                matchedEmployees.add(employee);
+        }
+
+        Assertions.assertEquals(matchedEmployees.size(), 2);
+        Assertions.assertEquals(matchedEmployees.get(0).getValue("birthDay"), "20220318");
+    }
+}
