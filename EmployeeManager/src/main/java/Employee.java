dc000ba0a8a9b0ed40d836b8225995677d1cbb8e
정병훈 <101618457+Harvey-JJ@users.noreply.github.com>
2022-03-18 11:05:52 +0900

    [Refactoring] 변수 변경 및 함수 수정
    1. 지윤님께서 리뷰해주신대로 key 값이 없을 경우 null을 return하도록 수정했습니다.
    2. set 함수를 추가했습니다.
    3. 옵션에 따른 검색 key 값을 employee에서 가지고 있게 되면 option 추가시마다 employee 수정이 필요하여 해당 부분을 option selector에 이관하기 위해 일부 column을 제거했습니다.

--- EmployeeManager/src/main/java/Employee.java
+++ EmployeeManager/src/main/java/Employee.java
@@ -5,13 +5,8 @@ public class Employee {
     private String id;
     public int sortKey;
     private String name;
-    private String firstName;
-    private String lastName;
     private String cl;
     private String phoneNumber;
-    private String firstNumber;
-    private String midNumber;
-    private String lastNumber;
     private String birthDay;
     private String certi;
 
@@ -21,13 +16,8 @@ public class Employee {
         this.id = id;
         this.sortKey = Integer.parseInt(id.substring(0, 2)) < 30 ? 2000000000 + Integer.parseInt(id) :  1900000000 + Integer.parseInt(id);
         this.name = name;
-        this.firstName = firstName;
-        this.lastName = lastName;
         this.cl = cl;
         this.phoneNumber = phoneNumber;
-        this.firstNumber = firstNumber;
-        this.midNumber = midNumber;
-        this.lastNumber = lastNumber;
         this.birthDay = birthDay;
         this.certi = certi;
     }
@@ -40,30 +30,42 @@ public class Employee {
         if(key == "name"){
             return name;
         }
-        if(key == "firstName"){
-            return firstName;
-        }
-        if(key == "lastName"){
-            return lastName;
-        }
         if(key == "cl"){
             return cl;
         }
         if(key == "phoneNumber"){
             return phoneNumber;
         }
-        if(key == "firstNumber"){
-            return firstNumber;
-        }
-        if(key == "midNumber"){
-            return midNumber;
-        }
-        if(key == "lastNumber"){
-            return lastNumber;
-        }
         if(key == "birthDay"){
             return birthDay;
         }
-        return certi;
+        if(key = ="certi"){
+            return certi;
+        }
+        return null;
+    }
+
+    public void setValue(String key, String value){
+        if(key == "name"){
+            this.name = value;
+            return;
+        }
+        if(key == "cl"){
+            this.cl = value;
+            return;
+        }
+        if(key == "phoneNumber"){
+            this.phoneNumber = value;
+            return;
+        }
+        if(key == "birthDay"){
+            this.birthDay = value;
+            return;
+        }
+        if(key == "certi"){
+            this.certi = value;
+            return;
+        }
+        return;
     }
 }
