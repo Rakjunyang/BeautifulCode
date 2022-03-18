0bb22e779483f65b54e8c89db3d3b6227dea2f5a
정병훈 <101618457+Harvey-JJ@users.noreply.github.com>
2022-03-18 11:25:34 +0900

    [fix] 함수 인자 bug-fiux
    함수 인자 bug-fiux

--- EmployeeManager/src/test/java/EmployeeTest.java
+++ EmployeeManager/src/test/java/EmployeeTest.java
@@ -6,37 +6,37 @@ public class EmployeeTest {
 
     @Test
     void idTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("id"), "12345678");
     }
 
     @Test
     void nameTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("name"), "first last");
     }
 
     @Test
     void clTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("cl"), "cl2");
     }
 
     @Test
     void phoneNumberTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("phoneNumber"), "010-1234-5678");
     }
 
     @Test
     void birthDayTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("birthDay"), "19920312");
     }
 
     @Test
     void certiTest(){
-        Employee employee = new Employee("12345678", "first last", "first", "last", "cl2", "010-1234-5678", "010", "1234", "5678", "19920312", "PRO");
+        Employee employee = new Employee("12345678", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
         Assertions.assertEquals(employee.getValue("certi"), "PRO");
     }
 }
