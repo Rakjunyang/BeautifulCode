b8ff145a280176bd651dd4355388aefdaa25437b
rakjun <lakjuny1@gmail.com>
2022-03-18 17:33:08 +0900

    중복 id 여부 체크 코드 추가

--- EmployeeManager/src/main/java/AddOperator.java
+++ EmployeeManager/src/main/java/AddOperator.java
@@ -19,12 +19,25 @@ public class AddOperator implements Operator {
         this.certi = certi;
     }
 
+    private boolean isExistsEmployee(ArrayList<Employee> employees) {
+        // 성능 개선 필요시 진행
+        for(Employee employee: employees){
+            if(employee.getValue("id").equals(employeeNum)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
-    public ArrayList<String> executeOperator(EmployeeManager employeeManager, OptionSelector optionSelector) {
+    public ArrayList<String> executeOperator(EmployeeManager employeeManager,
+        OptionSelector optionSelector) {
 
         Employee employee = new Employee(employeeNum, name, cl, phoneNum, birthday, certi);
+        if (isExistsEmployee(employeeManager.getEmployees())) {
+            return null;
+        }
         employeeManager.add(employee);
-
         return null;
     }
 }
