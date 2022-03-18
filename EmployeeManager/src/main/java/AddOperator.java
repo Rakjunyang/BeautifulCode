6787ee72cb06580a410eeeeb669db14dca3bb6b2
rakjun <lakjuny1@gmail.com>
2022-03-18 16:36:52 +0900

    Add Operator 구현

--- EmployeeManager/src/main/java/AddOperator.java
+++ EmployeeManager/src/main/java/AddOperator.java
@@ -1,14 +1,30 @@
 import java.util.ArrayList;
 
-public class AddOperator implements Operator{
+public class AddOperator implements Operator {
 
-    InputManager inputManager;
-    public AddOperator(InputManager inputManager){
-        this.inputManager = inputManager;
+    String employeeNum;
+    String name;
+    String cl;
+    String phoneNum;
+    String birthday;
+    String certi;
+
+    public AddOperator(String employeeNum, String name, String cl, String phoneNum, String birthday,
+        String certi) {
+        this.employeeNum = employeeNum;
+        this.name = name;
+        this.cl = cl;
+        this.phoneNum = phoneNum;
+        this.birthday = birthday;
+        this.certi = certi;
     }
 
     @Override
-    public String executeOperator() {
+    public ArrayList<String> executeOperator(EmployeeManager employeeManager, OptionSelector optionSelector) {
+
+        Employee employee = new Employee(employeeNum, name, cl, phoneNum, birthday, certi);
+        employeeManager.add(employee);
+
         return null;
     }
 }
