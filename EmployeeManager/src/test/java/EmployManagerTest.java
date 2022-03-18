fc81590d6c13f7c4c2ccfb1359bbf90a1c8b59f1
정병훈 <101618457+Harvey-JJ@users.noreply.github.com>
2022-03-18 15:52:20 +0900

    [Feature] EmployeeManager Test Code 추가
    EmployeeManager Test Code 추가

--- EmployeeManager/src/test/java/EmployManagerTest.java
+++ EmployeeManager/src/test/java/EmployManagerTest.java
@@ -1,55 +1,65 @@
-import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 public class EmployManagerTest {
-/*
+
     EmployeeManager employeeManager;
     OptionSelector optionSelector;
 
     @BeforeEach
     void setUp() {
-        optionSelector = mock(OptionSelector.class);
-        when(optionSelector.match(any(Employee.class))).thenReturn(true);
 
         employeeManager = new EmployeeManager();
-        employeeManager.add("12345600", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
-        employeeManager.add("12345601", "abc def",  "cl3", "010-1111-2222", "20120101", "ADV");
-        employeeManager.add("00111234", "one man",  "cl4", "010-2222-3333", "19700000", "ADV");
+        Employee employee1 = new Employee("12345600", "first last",  "cl2", "010-1234-5678", "19920312", "PRO");
+        Employee employee2 = new Employee("12345601", "abc def",  "cl3", "010-1111-2222", "20120101", "ADV");
+        Employee employee3 = new Employee("00111234", "one man",  "cl4", "010-2222-3333", "19700000", "ADV");
+        employeeManager.add(employee1);
+        employeeManager.add(employee2);
+        employeeManager.add(employee3);
     }
 
     @Test
+    void getEmployeeTest(){
+        ArrayList<Employee> employees = employeeManager.getEmployees();
+        Assertions.assertNotNull(employees);
+    }
+    
+    @Test
     void addTest(){
-        int size = employeeManager.employees.size();
-        employeeManager.add("00111235", "one man",  "cl4", "010-2222-3333", "19700000", "ADV");
-        Assertions.assertEquals(employeeManager.employees.size(), size + 1);
+        int size = employeeManager.getEmployees().size();
+        Employee employee = new Employee("00111235", "one man",  "cl4", "010-2222-3333", "19700000", "ADV");
+        employeeManager.add(employee);
+        Assertions.assertEquals(employeeManager.getEmployees().size(), size + 1);
     }
 
     @Test
     void delTest(){
-        Assertions.assertEquals(employeeManager.del(optionSelector).size(), 3);
-        Assertions.assertEquals(employeeManager.employees.size(), 0);
+        ArrayList<Employee> employees = employeeManager.getEmployees();
+        int size = employees.size();
+        String id = employees.get(0).getValue("id");
+        employeeManager.del(0);
+        Assertions.assertEquals(employees.size(), size -1);
+        
+        
+        for(Employee employee : employees)
+        {
+            Assertions.assertNotEquals(employee.getValue("id"), id);
+        }
     }
 
     @Test
-    void modTest(){
-        ArrayList<Employee> list = employeeManager.mod(optionSelector, "certi", "ADV");
-        Assertions.assertEquals(list.size(), 3);
-        Assertions.assertEquals(list.get(0).getValue("certi"), "PRO");
-        Assertions.assertEquals(list.get(0).getValue("id"), "12345600");
-        Assertions.assertEquals(employeeManager.employees.get(0).getValue("certi"), "ADV");
-    }
-
-    @Test
-    void schTest(){
-        ArrayList<Employee> list = employeeManager.sch(optionSelector);
-        Assertions.assertEquals(list.size(), 3);
-    }
-
- */
+    void sortTest(){
+        ArrayList<Employee> employees = employeeManager.getEmployees();
+        employeeManager.sort();
+        int firstId = Integer.parseInt(employees.get(0).getValue("id"));
+        int firstSortId = firstId < 30000000 ? 2000000000 + firstId : 1900000000 + firstId;
+        int secondId = Integer.parseInt(employees.get(1).getValue("id"));
+        int secondSortId = secondId < 30000000 ? 2000000000 + secondId : 1900000000 + secondId;
+        
+        Assertions.assertTrue(firstSortId < secondSortId);
+    } 
 
 }
