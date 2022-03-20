d4df6bc4b5511c390cd44eeeb8c3ce76edf2f2a3
rakjun <lakjuny1@gmail.com>
2022-03-20 21:47:28 +0900

    add deloperatortest case

--- EmployeeManager/src/test/java/OperatorTest.java
+++ EmployeeManager/src/test/java/OperatorTest.java
@@ -12,14 +12,15 @@ import org.junit.jupiter.api.Test;
 public class OperatorTest {
 
     static Employee employee;
+    static Employee employee2;
     static OptionSelector optionSelector;
     @BeforeAll
     static void setEmployee(){
         employee = new Employee("08117441","BMU MPOSXU","CL3","010-2703-3153","20010215","ADV");
+        employee2 = new Employee("96548512","ASER ZCDEU","CL2","010-2124-5333","19780721","ADV");
         optionSelector = mock(OptionSelector.class);
         when(optionSelector.match(employee)).thenReturn(true);
-
-
+        when(optionSelector.match(employee2)).thenReturn(true);
     }
 
     @Test
@@ -36,8 +37,12 @@ public class OperatorTest {
         matchedStrList.add("DEL,"+ employee.getValue("id") + "," + employee.getValue("NAME") + ","
             + employee.getValue("CL") + "," + employee.getValue("PHONENUMBER") + "," + employee.getValue(
             "BIRTHDAY") + "," + employee.getValue("CERTI"));
+        matchedStrList.add("DEL,"+ employee2.getValue("id") + "," + employee2.getValue("NAME") + ","
+            + employee2.getValue("CL") + "," + employee2.getValue("PHONENUMBER") + "," + employee2.getValue(
+            "BIRTHDAY") + "," + employee2.getValue("CERTI"));
 
         employeeManager.add(employee);
+        employeeManager.add(employee2);
         Operator deleteOperator = new DeleteOperator();
 
         assertEquals(deleteOperator.executeOperator(employeeManager,optionSelector),matchedStrList);
