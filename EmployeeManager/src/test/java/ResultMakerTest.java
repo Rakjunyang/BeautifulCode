306229d245f76e56384bdd402a2796ca5da1c7cc
rakjun <lakjuny1@gmail.com>
2022-03-21 16:07:41 +0900

    add NormalResultMakerNotZeroTest case

--- EmployeeManager/src/test/java/ResultMakerTest.java
+++ EmployeeManager/src/test/java/ResultMakerTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,18 @@ public class ResultMakerTest {
         POption.add(false);
         ResultMaker resultMaker = new ResultMakerFactory().getResultMaker(POption);
         assertEquals(resultMaker.getResult(matchedEmployeeList, "MOD"),"MOD,NONE");
+    }
+
+    @Test
+    void NormalResultMakerNotZeroTest() {
+        ArrayList<Boolean> POption = new ArrayList<>();
+        ArrayList<String> matchedEmployeeList = new ArrayList<>();
+        matchedEmployeeList.add(anyString());
+        matchedEmployeeList.add(anyString());
+        matchedEmployeeList.add(anyString());
+        POption.add(false);
+        ResultMaker resultMaker = new ResultMakerFactory().getResultMaker(POption);
+        assertEquals(resultMaker.getResult(matchedEmployeeList, "MOD"),"MOD,3");
     }
 
 }
