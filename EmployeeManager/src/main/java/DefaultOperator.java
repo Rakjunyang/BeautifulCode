c7859ded609dbb92094c01b0ef221c7064ef4f7e
rakjun <lakjuny1@gmail.com>
2022-03-21 10:39:29 +0900

    add DefaultOperator abstract class - it have getMatchedString method

---
+++ EmployeeManager/src/main/java/DefaultOperator.java
@@ -0,0 +1,13 @@
+public abstract class DefaultOperator implements Operator{
+    private String operatorName;
+
+    public void setOperatorName(String operatorName){
+        this.operatorName = operatorName;
+    }
+
+    public String getMatchedString(Employee employee) {
+        return this.operatorName + employee.getValue("id") + "," + employee.getValue("NAME") + ","
+            + employee.getValue("CL") + "," + employee.getValue("PHONENUMBER") + "," + employee.getValue(
+            "BIRTHDAY") + "," + employee.getValue("CERTI");
+    }
+}
