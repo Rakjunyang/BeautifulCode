b477111731718ee628ed5986fe6d1ef718dc2f14
jiyun <wldbs9211@nate.com>
2022-03-21 11:28:24 +0900

    feat: Add parser valid test

--- EmployeeManager/src/main/java/Parser.java
+++ EmployeeManager/src/main/java/Parser.java
@@ -3,12 +3,50 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public static ArrayList<String> parse(String str) {
+    private static boolean isValidPattern(String str){
         String validPattern = "^[a-zA-Z0-9\\s\\,\\-]+$";
+        return Pattern.matches(validPattern, str);
+    }
 
-        if (Pattern.matches(validPattern, str)){
-            return new ArrayList<>(Arrays.asList(str.split(",")));
+    private static boolean isValidOperation(String operation){
+        ArrayList<String> supportType = new ArrayList<>(Arrays.asList("ADD", "SCH", "MOD", "DEL"));
+        return supportType.contains(operation.toUpperCase());
+    }
+
+    private static boolean isValidLength(ArrayList<String> inputList, int optionLength){
+        String operation = inputList.get(0).toUpperCase();
+
+        // TODO: 컬럼이 추가되지는 않을까요? parser가 아닌 데이터를 관리하는 쪽으로 이동?
+        if (operation.equals("ADD")) return inputList.size() == (1 + optionLength + 6);
+        else if (operation.equals("SCH")) return inputList.size() == (1 + optionLength + 2);
+        else if (operation.equals("MOD")) return inputList.size() == (1 + optionLength + 4);
+        else if (operation.equals("DEL")) return inputList.size() == (1 + optionLength + 2);
+
+        return false;
+    }
+
+    private static boolean isValidOption(ArrayList<String> inputList, ArrayList<ArrayList<String>> supportOptions){
+        int optionIndex = 1;
+        for (ArrayList<String> supportOption: supportOptions){
+            if (!supportOption.contains(inputList.get(optionIndex))) return false;
+            optionIndex++;
         }
-        return null;
+        return true;
+    }
+
+    public static ArrayList<String> parse(String str) {
+        ArrayList<String> parseResult = new ArrayList<>(Arrays.asList(str.split(",")));
+
+        ArrayList<ArrayList<String>> supportOptions = new ArrayList<>();
+        supportOptions.add(new ArrayList<>(Arrays.asList("-p", " ")));
+        supportOptions.add(new ArrayList<>(Arrays.asList("-f", "-l", "-m", "-y", "-d", " ")));
+        supportOptions.add(new ArrayList<>(Arrays.asList(" ")));
+
+        if (!isValidPattern(str)) return null;
+        if (!isValidOperation(parseResult.get(0))) return null;
+        if (!isValidLength(parseResult, supportOptions.size())) return null;
+        if (!isValidOption(parseResult, supportOptions)) return null;
+
+        return parseResult;
     }
 }
--- EmployeeManager/src/test/java/ParserTest.java
+++ EmployeeManager/src/test/java/ParserTest.java
@@ -1,18 +1,67 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 public class ParserTest {
 
     @Test
     void validStringTest() {
-        assertNotNull(new Parser().parse("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
-        assertNotNull(new Parser().parse("SCH,-p,-d, ,birthday,04"));
-        assertNotNull(new Parser().parse("DEL, , , ,employeeNum,18115040"));
-        assertNotNull(new Parser().parse("MOD,-p, , ,employeeNum,08123556,birthday,20110706"));
+        ArrayList<String> parseResult = Parser.parse("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
+        assertEquals(parseResult.get(0), "ADD");
+        assertEquals(parseResult.get(1), " ");
+        assertEquals(parseResult.get(2), " ");
+        assertEquals(parseResult.get(3), " ");
+        assertEquals(parseResult.get(4), "15123099");
+        assertEquals(parseResult.get(5), "VXIHXOTH JHOP");
+        assertEquals(parseResult.get(6), "CL3");
+        assertEquals(parseResult.get(7), "010-3112-2609");
+        assertEquals(parseResult.get(8), "19771211");
+        assertEquals(parseResult.get(9), "ADV");
 
-        assertNull(new Parser().parse("DEL, , , ,name,홍길동"));
-        assertNull(new Parser().parse("DEL, , , ,employeeNum,18115040@"));
+        parseResult = Parser.parse("SCH,-p,-d, ,birthday,04");
+        assertEquals(parseResult.get(0), "SCH");
+        assertEquals(parseResult.get(1), "-p");
+        assertEquals(parseResult.get(2), "-d");
+        assertEquals(parseResult.get(3), " ");
+        assertEquals(parseResult.get(4), "birthday");
+        assertEquals(parseResult.get(5), "04");
+
+        assertNotNull(Parser.parse("DEL, , , ,employeeNum,18115040"));
+        assertNotNull(Parser.parse("MOD,-p, , ,employeeNum,08123556,birthday,20110706"));
+    }
+
+    @Test
+    void invalidStringTest() {
+        assertNull(Parser.parse("DEL, , , ,name,홍길동"));
+        assertNull(Parser.parse("DEL, , , ,employeeNum,18115040@"));
+    }
+
+    @Test
+    void operationTypeTest() {
+        assertNotNull(Parser.parse("DEL, , , ,employeeNum,18115040"));
+        assertNull(Parser.parse("InvalidOperation, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
+    }
+
+    @Test
+    void dataLengthTest() {
+        assertNotNull(Parser.parse("DEL, , , ,employeeNum,18115040"));
+        assertNull(Parser.parse("DEL, , , ,employeeNum"));
+        assertNull(Parser.parse("MOD,-p, , ,employeeNum,08123556"));
+        assertNull(Parser.parse("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211"));
+        assertNull(Parser.parse("SCH,-p,-d, ,birthday"));
+    }
+
+    @Test
+    void optionTest() {
+        assertNotNull(Parser.parse("SCH,-p,-d, ,birthday,04"));
+        assertNotNull(Parser.parse("SCH, , , ,birthday,04"));
+
+        assertNull(Parser.parse("SCH,-z, , ,birthday,04"));
+        assertNull(Parser.parse("SCH, ,-x, ,birthday,04"));
+        assertNull(Parser.parse("SCH, , ,-y,birthday,04"));
+        assertNull(Parser.parse("SCH,, , ,birthday,04"));
     }
 }
