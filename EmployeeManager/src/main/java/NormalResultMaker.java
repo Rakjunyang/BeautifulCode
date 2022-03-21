6bfd236f7176c4b07bb543f866baec0558028151
rakjun <lakjuny1@gmail.com>
2022-03-21 16:09:22 +0900

    dev Normal getResult

--- EmployeeManager/src/main/java/NormalResultMaker.java
+++ EmployeeManager/src/main/java/NormalResultMaker.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NormalResultMaker implements ResultMaker {
 
-    public NormalResultMaker(){
+    public NormalResultMaker() {
 
     }
+
+
     @Override
-    public String getResult() {
-        return null;
+    public String getResult(ArrayList<String> matchedEmployeeList, String operatorName) {
+
+        if (matchedEmployeeList.isEmpty() || matchedEmployeeList.size() == 0) {
+            return operatorName + "," + "NONE";
+        }
+        return operatorName + "," + matchedEmployeeList.size();
     }
 }
